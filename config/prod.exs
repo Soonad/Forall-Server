6980a5cabbd31a3516de82cdb5429257f2a36bd7
Bernardo Amorim <contato@bamorim.com>
2019-11-21 18:31:24 -0300

    feat: use psql over ssl on prod

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,7 @@ import Config
 
 config :forall, Forall.Repo,
   show_sensitive_data_on_connection_error: false,
+  ssl: true,
   pool_size: 10
 
 config :forall, ForallWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
